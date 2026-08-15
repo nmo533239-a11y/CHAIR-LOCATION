67399fca68d379076af19c87072a5d5b17bcf075
nmo533239-a11y <nmo533239@gmail.com>
2026-08-15 12:09:32 -0700

    fix navbar import path

--- app/components/Subscribe.jsx
+++ app/components/Subscribe.jsx
@@ -5,7 +5,7 @@ export default function Subscribe(){
             <div className="relative w-full h-[600px]">
       <Image src="/last.jpg" alt="last logo" fill className="object-cover"/>
             </div>
-            <span className="text-4xl font-bold tracking-wide">Subscribe To Our Newsletter</span>
+            <span className="text-4xl font-bold tracking-wide ">Subscribe To Our Newsletter</span>
             <p className="w-2/3">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tenetur architecto earum exercitationem. Illo impedit facere commodi obcaecati quod quos in iste aliquid error ex, ipsam modi, laudanti</p>
  <div className="flex gap-4 p-2">
     <input placeholder="write your mail" className=" rounded-md w-96 focus:outline-none border py-1 px-6"/>
